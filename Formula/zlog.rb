24e6eb41d078a86e14eb1524757ed2d94aa49c88
Xun Zhu <zhuxun2@gmail.com>
2026-01-12 14:08:38 -0600

    Fix v0.1.1 checksum

--- Formula/zlog.rb
+++ Formula/zlog.rb
@@ -2,7 +2,7 @@ class Zlog < Formula
   desc "Lightweight CLI tool with browser UI for streaming NDJSON logs"
   homepage "https://github.com/w9/zlog"
   url "https://github.com/w9/zlog/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "90969c3a694953eb23b7dedd79e6aac50371103edf94325da012bfb2fe68b221"
+  sha256 "e11f3b37e9aadbca45740c9c496d6e14ce1b6f75769209e3b7f7877c2245bf28"
   license "MIT"
 
   depends_on "go" => :build
